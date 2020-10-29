fa7f5aedf8a0c067bdafb2e4ab6e268f470a1893
Aaron Contreras <aaronlcaq@gmail.com>
2020-10-29 12:55:04 -0500

    Complete form based workflow with flash notices

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -17,14 +17,31 @@ class ArticlesController < ApplicationController
 
     @article.save
 
+    flash.notice = "Article '#{@article.title}' created!"
+
     redirect_to article_path(@article)
   end
 
   def destroy
-    article = Article.find(params[:id])
+    @article = Article.find(params[:id])
 
-    article.destroy
+    @article.destroy
+
+    flash.notice = "Article '#{@article.title}' deleted!"
 
     redirect_to articles_path
+  end
+
+  def edit
+    @article = Article.find(params[:id])
+  end
+  
+  def update
+    @article = Article.find(params[:id])
+    @article.update(article_params)
+
+    flash.notice = "Article '#{@article.title}' updated!"
+
+    redirect_to article_path(@article)
   end
 end
